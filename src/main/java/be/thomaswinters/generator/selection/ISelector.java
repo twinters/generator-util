6c9f00e28f538115ed6ed7a6c435bf178e3f5c8d
T. Winters <info@thomaswinters.be>
2018-04-19 20:20:23 +0200

    Added selector interface

---
+++ src/main/java/be/thomaswinters/generator/selection/ISelector.java
@@ -0,0 +1,12 @@
+package be.thomaswinters.generator.selection;
+
+import java.util.Collection;
+import java.util.List;
+
+public interface ISelector<E> {
+    default E select(List<E> collection) {
+        return select(collection, 1).iterator().next();
+    }
+
+    Collection<E> select(List<E> collection, int amount);
+}
