6f48b3f99aefb9b1751954b8cf8480dd90b72bf4
T. Winters <info@thomaswinters.be>
2018-04-20 19:29:54 +0200

    Made IGenerator extend Supplier<Optional>

--- src/main/java/be/thomaswinters/generator/generators/IGenerator.java
+++ src/main/java/be/thomaswinters/generator/generators/IGenerator.java
@@ -1,7 +1,13 @@
 package be.thomaswinters.generator.generators;
 
 import java.util.Optional;
+import java.util.function.Supplier;
 
-public interface IGenerator<E> {
+public interface IGenerator<E> extends Supplier<Optional<E>> {
     Optional<E> generate();
+
+    @Override
+    default Optional<E> get() {
+        return generate();
+    }
 }
