e58b7fc9865d32cdce58d87fb8dc17053c5c998a
T. Winters <info@thomaswinters.be>
2018-04-24 04:50:01 +0200

    Fixed bug with AMaximisingGenerator

--- src/main/java/be/thomaswinters/generator/stream/AMaximisingGenerator.java
+++ src/main/java/be/thomaswinters/generator/stream/AMaximisingGenerator.java
@@ -1,14 +1,12 @@
 package be.thomaswinters.generator.stream;
 
 import be.thomaswinters.generator.generators.IGenerator;
-import be.thomaswinters.generator.generators.RetryingGenerator;
 
 import java.util.Comparator;
 import java.util.Optional;
 import java.util.function.Supplier;
-import java.util.stream.Stream;
 
-public class AMaximisingGenerator<E, F extends Supplier<Optional<E>>> extends RetryingGenerator<E, F> implements IGenerator<E> {
+public class AMaximisingGenerator<E, F extends Supplier<Optional<E>>> extends ARetryingGenerator<E, F> implements IGenerator<E> {
     private Comparator<E> comparator;
 
     public AMaximisingGenerator(F innerGenerator, int maxTrials, Comparator<E> comparator) {
@@ -24,9 +22,7 @@ public class AMaximisingGenerator<E, F extends Supplier<Optional<E>>> extends Re
     }
 
     protected Optional<E> maximise(Supplier<Optional<E>> generator) {
-        return Stream.generate(generator)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
+        return createRetryingStream(generator)
                 .max(comparator);
     }
 }
