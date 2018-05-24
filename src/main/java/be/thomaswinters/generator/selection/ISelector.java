c44c5ffbad11d4b4aac36674124373de1a611229
T. Winters <info@thomaswinters.be>
2018-05-24 14:13:30 +0200

    Added converter between selector and generator

--- src/main/java/be/thomaswinters/generator/selection/ISelector.java
+++ src/main/java/be/thomaswinters/generator/selection/ISelector.java
@@ -1,5 +1,7 @@
 package be.thomaswinters.generator.selection;
 
+import be.thomaswinters.generator.generators.IGenerator;
+
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Optional;
@@ -7,6 +9,10 @@ import java.util.stream.Stream;
 
 @FunctionalInterface
 public interface ISelector<E> {
+    default IGenerator<E> toGenerator(Stream<E> collection) {
+        return ()->select(collection);
+    }
+
     default Optional<E> select(Stream<E> collection) {
         Iterator<E> it = select(collection, 1).iterator();
         return it.hasNext() ? Optional.of(it.next()) : Optional.empty();
