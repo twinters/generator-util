9a021c7129d9d2ad994b7b26b80fe30fab8a5e71
Thomas Winters <info@thomaswinters.be>
2018-08-09 14:46:33 +0200

    Added new generator combiner constructors

--- src/main/java/be/thomaswinters/generator/generators/GeneratorCombiner.java
+++ src/main/java/be/thomaswinters/generator/generators/GeneratorCombiner.java
@@ -4,14 +4,30 @@ import be.thomaswinters.generator.selection.RouletteWheelSelection;
 import be.thomaswinters.generator.selection.Weighted;
 import com.google.common.collect.ImmutableList;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class GeneratorCombiner<E> implements IGenerator<E> {
     private final ImmutableList<Weighted<IGenerator<E>>> generators;
 
-    public GeneratorCombiner(ImmutableList<Weighted<IGenerator<E>>> generators) {
-        this.generators = generators;
+    public GeneratorCombiner(List<Weighted<IGenerator<E>>> generators) {
+        this.generators = ImmutableList.copyOf(generators);
+    }
+
+    public static <E> GeneratorCombiner<E> fromGenerators(List<IGenerator<E>> generators) {
+        return new GeneratorCombiner<>(
+                generators
+                        .stream()
+                        .map(e -> new Weighted<>(e, 1))
+                        .collect(Collectors.toList()));
+    }
+
+    @SafeVarargs
+    public static <E> GeneratorCombiner<E> fromGenerators(IGenerator<E>... generators) {
+        return fromGenerators(Arrays.asList(generators));
     }
 
     @Override
